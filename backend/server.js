9607730c28051712170ee40b8a0e7913c22cd075
aliff <alifzulkifeli@gmail.com>
2020-10-10 10:07:21 +0900

    settle up the auth back end

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,13 @@ import connectDB from "./config/db.js";
 import colors from "colors";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 connectDB();
+
 const app = express();
+app.use(express.json());
 morgan("tiny");
 
 app.get("/", (req, res) => {
@@ -16,6 +19,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
